148018f6cf1a8ad10c4b1be5f65af40c69c2ac89
Kruithne <kruithne@gmail.com>
2025-05-21 10:12:58 +0100

    implement index and static asset routing

--- server.ts
+++ server.ts
@@ -1,9 +1,50 @@
 import { serve, caution, HTTP_STATUS_CODE } from 'spooder';
+import path from 'node:path';
+import crypto from 'node:crypto';
+import { format } from 'node:util';
 
 const server = serve(Number(process.env.SERVER_PORT), process.env.SERVER_LISTEN_HOST);
 
-server.route('/', (req, url) => {
-	return 'Hello from the new server!';
+export function log(message: string, ...args: unknown[]): void {
+	let formatted_message = format('[info] ' + message, ...args);
+	
+	// Replace all {...} with text wrapped in ANSI color code 13.
+	formatted_message = formatted_message.replace(/\{([^}]+)\}/g, '\x1b[38;5;13m$1\x1b[0m');
+
+	console.log(formatted_message);
+}
+
+let index: string|null = null;
+let index_hash: string|null = null;
+
+server.route('/', async (req, url) => {
+	if (index === null) {
+		index = await Bun.file('./html/index.html').text();
+		index_hash = crypto.createHash('sha256').update(index).digest('hex');
+	}
+
+	const headers = {
+		'Content-Type': 'text/html',
+		'Access-Control-Allow-Origin':  '*',
+		'ETag': index_hash as string
+	} as Record<string, string>;
+
+	if (req.headers.get('If-None-Match') === index_hash)
+		return new Response(null, { status: 304, headers }); // Not Modified
+
+	return new Response(index, { status: 200, headers });
+});
+
+server.dir('/static', './static', async (file_path, file, stat, request) => {
+	// ignore hidden files
+	if (path.basename(file_path).startsWith('.'))
+		return 404; // Not Found
+
+	// ignore directories
+	if (stat.isDirectory())
+		return 401; // Unauthorized
+
+	return file;
 });
 
 async function default_handler(status_code: number): Promise<Response> {
