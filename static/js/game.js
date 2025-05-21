1f8c4b31679db407554334c3e877356170eb7ffb
Kruithne <kruithne@gmail.com>
2025-05-21 14:07:28 +0100

    further prototype for rewrite

--- static/js/game.js
+++ static/js/game.js
@@ -25,12 +25,34 @@ async function document_load() {
 				current_round: 0,
 				remaining_lives: MAX_LIVES,
 				player_guesses: [],
+				current_location: null,
+
+				viewing_map: false,
 			}
 		},
 
 		computed: {
 			player_accuracy() {
 				return 0; // todo
+			},
+
+			is_alive() {
+				return this.remaining_lives > 0;
+			},
+
+			current_location_background() {
+				if (this.current_location === null)
+					return '';
+
+				return 'static/images/' + this.location_dir + '/' + this.current_location.id + '.jpg';
+			},
+
+			location_dir() {
+				return this.is_classic ? 'locations_classic' : 'locations';
+			},
+
+			tiles_dir() {
+				return this.is_classic ? 'tiles_classic' : 'tiles';
 			}
 		},
 
@@ -48,8 +70,7 @@ async function document_load() {
 			},
 
 			async load_location_data() {
-				const location_file = this.is_classic ? 'locations_classic' : 'locations'
-				const response = await fetch(`static/data/${location_file}.json`);
+				const response = await fetch(`static/data/${this.location_dir}.json`);
 				const content = await response.json();
 
 				const locations = [];
@@ -70,17 +91,40 @@ async function document_load() {
 				});
 			
 				this.map.setView([-120.90349875311426, 124.75], 2);
-			
-				const dir = this.is_classic ? 'tiles_classic' : 'tiles';
-				L.tileLayer('static/images/' + dir + '/{z}/{x}/{y}.png', { maxZoom: this.is_classic ? 6 : 7 }).addTo(this.map);
+				L.tileLayer('static/images/' + this.tiles_dir + '/{z}/{x}/{y}.png', { maxZoom: this.is_classic ? 6 : 7 }).addTo(this.map);
 			},
 
 			reset_game_state() {
-				// todo: copy locations to location_pool
+				const new_pool = Array(this.locations.length);
+				for (let i = 0, n = this.locations.length; i < n; i++)
+					new_pool[i] = i;
+
+				this.location_pool = new_pool;
+
 				this.current_round = 0;
 				this.remaining_lives = MAX_LIVES;
 
 				this.player_guesses.length = 0;
+				this.viewing_map = false;
+			},
+
+			next_round() {
+				if (!this.is_alive)
+					return; // todo: show gameover screen
+
+				if (this.location_pool.length === 0)
+					return; // todo: show game complete screen
+
+				this.current_round++;
+
+				const new_location_idx = Math.floor(Math.random() * this.location_pool.length);
+				this.current_location = this.location_pool.splice(new_location_idx, 1)[0];
+
+				this.update_panorama();
+			},
+
+			update_panorama() {
+
 			}
 		}
 	}).mount('#container');
